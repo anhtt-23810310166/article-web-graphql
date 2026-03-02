62e1541aeffe8c1c121f5e8bc5167b50bd18b49d
Trần Tuấn Anh <91252961+khaihuen@users.noreply.github.com>
2026-03-02 19:59:19 +0700

    Lấy dữ liệu bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,11 +1,21 @@
 import Article from "./models/article.model";
 
- const resolvers = {
-    Query: {
-      articles: async () => {
-        return await Article.find({ deleted: false });
-      },
+const resolvers = {
+  Query: {
+    articles: async () => {
+      return await Article.find({ deleted: false });
     },
-  };
+    article: async (_: any, args: { id: string }) => {
+      return await Article.findOne({ _id: args.id, deleted: false });
+    },
+  },
+  Mutation: {
+    createArticle: async (_: any, args: any) => {
+      const article = new Article(args);
+      await article.save();
+      return article;
+    },
+  },
+};
 
-  export default resolvers
+export default resolvers;
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,13 +1,19 @@
-const typeDefs = `
+const typeDefs = `#graphql
     type Query {
         articles: [Article]
+        article(id: ID!): Article
+    }
+
+    type Mutation {
+        createArticle(title: String, description: String, avatar: String): Article
     }
 
     type Article {
         id: ID!
-        title: String!
-        content: String!
-        deleted: Boolean!
+        title: String
+        description: String
+        avatar: String
+        deleted: Boolean
     }
-`
-export default typeDefs
+`;
+export default typeDefs;
